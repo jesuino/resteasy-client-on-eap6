edd2eab686f2a46b5d1a1e4a166be8319b283cbb
jesuino <william.a.siqueira@gmail.com>
2014-07-02 16:13:52 -0300

    Adding RESTEasy Client

--- src/main/java/org/jboss/as/quickstarts/jaxrsclient/SearchServlet.java
+++ src/main/java/org/jboss/as/quickstarts/jaxrsclient/SearchServlet.java
@@ -1,5 +1,7 @@
 package org.jboss.as.quickstarts.jaxrsclient;
 
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -11,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/searchCep")
 public class SearchServlet extends HttpServlet {
 
-	final String WS_URL = "http://correiosapi.apphb.com/cep/{CEP}";	
+	final String WS_URL = "http://correiosapi.apphb.com/cep/{cep}";	
 
 	final String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
 	final String PAGE_FOOTER = "</body></html>";
@@ -42,16 +44,25 @@ public class SearchServlet extends HttpServlet {
 			content += "<h1>Information for CEP \"" + cep + "\":</h1>";
 			content += "<b>Logradouro:</b> " + cepResponse.getLogradouro() + "<br />";
 			content += "<b>Tipo Logradouro:</b> " + cepResponse.getTipoDeLogradouro() + "<br />";
-			content += "<b>Bairro</b> " + cepResponse.getBairro() + "<br />";
-			content += "<b>Cidade</b> " + cepResponse.getCidade() + "<br />";
+			content += "<b>Bairro:</b> " + cepResponse.getBairro() + "<br />";
+			content += "<b>Cidade:</b> " + cepResponse.getCidade() + "<br />";
 		}
-
 		return content;
 
 
 	}
 
 	public CepResponse getCepData(String cep){
-		return null;
+		try{
+			ClientRequest request = new ClientRequest(WS_URL);
+			request.accept("application/json");
+			request.pathParameter("cep", cep);
+			ClientResponse<CepResponse> response = request.get(CepResponse.class);
+			return response.getEntity();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 }
